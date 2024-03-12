e081e6e2f9082dcef1433389ad2eccf704a2cbc0
uvictory <tmdfl1067@naver.com>
2024-03-13 03:54:50 +0900

    유승리 2017112124

--- prac.py
+++ prac.py
@@ -1,8 +1,8 @@
 def main():
     # 이름과 학번 입력 받기
-    name = input("이름을 입력하세요: ")
-    student_id = input("학번을 입력하세요: ")
-    major = input("학과를 입력하세요: ")
+    name = input("이름을   입력하세요: ")
+    student_id = input("학번을   입력하세요: ")
+    major = input("학과를   입력하세요: ")
 
     # 입력 받은 정보 출력
     print("입력한 정보:")
