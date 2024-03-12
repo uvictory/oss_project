bf80d237171a313901f914290549bbd534e78e06
uvictory <tmdfl1067@naver.com>
2024-03-13 03:37:40 +0900

    oss_project에 commit하는 메세지입니다.

---
+++ prac.py
@@ -0,0 +1,13 @@
+def main():
+    # 이름과 학번 입력 받기
+    name = input("이름을 입력하세요: ")
+    student_id = input("학번을 입력하세요: ")
+
+    # 입력 받은 정보 출력
+    print("입력한 정보:")
+    print("이름:", name)
+    print("학번:", student_id)
+
+if __name__ == "__main__":
+    main()
+    
